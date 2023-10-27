2f664115b05eef985d79aa39fab1cba5d4fb1ba5
girjeshbaghel <girjesh62586@gmail.com>
2023-10-27 12:08:24 +0530

    Demo Project is Push

---
+++ src/main/java/com/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+    @GetMapping("/")
+    public String demo1()
+    {
+        return "Project Running";
+    }
+}
