0ba6422bc79d8e487ab6a694e9d15e474db05358
girjeshbaghel <girjesh62586@gmail.com>
2023-10-27 16:07:52 +0530

    Jenkins Another CLass add1

--- src/main/java/com/controller/JenkinsAnotherClass.java
+++ src/main/java/com/controller/JenkinsAnotherClass.java
@@ -6,6 +6,8 @@ public class JenkinsAnotherClass {
     public void temp()
     {
         System.out.println("Jenkins Another is Running 1");
+        System.out.println("Jenkins Another is Running 2");
+        System.out.println("Jenkins Another is Running 3");
 
 
 
